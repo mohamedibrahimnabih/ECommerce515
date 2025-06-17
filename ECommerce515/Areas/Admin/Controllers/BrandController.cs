6b9832cd6b2f3dedb19f18e24028aadbeebb93cb
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-17 22:06:58 +0300

    CRUD Operation on Brand Model

---
+++ ECommerce515/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce515.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var brands = _context.Brands;
+
+            return View(brands.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Brand brand)
+        {
+            _context.Add(brand);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit([FromRoute] int id)
+        {
+            var brand = _context.Brands.Find(id);
+
+            if(brand is not null)
+            {
+                return View(brand);
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Brand brand)
+        {
+            _context.Update(brand);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var brand = _context.Brands.Find(id);
+
+            if (brand is not null)
+            {
+                _context.Remove(brand);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+    }
+}
