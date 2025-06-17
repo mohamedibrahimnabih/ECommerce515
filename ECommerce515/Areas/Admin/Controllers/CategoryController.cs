2332af87ab58a1d156a9a37cef2f317f25e630e6
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-17 21:27:08 +0300

    CRUD Operation on Category Model

---
+++ ECommerce515/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce515.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories;
+
+            return View(categories.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Add(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit([FromRoute] int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if(category is not null)
+            {
+                return View(category);
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _context.Update(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category is not null)
+            {
+                _context.Remove(category);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+    }
+}
--- ECommerce515/Areas/Admin/Controllers/HomeController.cs
+++ ECommerce515/Areas/Admin/Controllers/HomeController.cs
@@ -9,5 +9,10 @@ namespace ECommerce515.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult NotFoundPage()
+        {
+            return View();
+        }
     }
 }
