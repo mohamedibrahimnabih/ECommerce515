6acbe5bdd5bf4f9cabf04704305862917b835cc9
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-17 23:02:39 +0300

    CRUD Operation on Product Model

---
+++ ECommerce515/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce515.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.Include(e => e.Category).Include(e => e.Brand);
+
+            return View(products.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var categories = _context.Categories;
+            var brands = _context.Brands;
+
+            CategoryWithBrandVM categoryWithBrandVM = new()
+            {
+                Categories = categories.ToList(),
+                Brands = brands.ToList()
+            };
+
+            return View(categoryWithBrandVM);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int productId)
+        {
+            var product = _context.Products.Find(productId);
+
+            if(product is not null)
+            {
+                var categories = _context.Categories;
+                var brands = _context.Brands;
+
+                CategoryWithBrandVM categoryWithBrandVM = new()
+                {
+                    Categories = categories.ToList(),
+                    Brands = brands.ToList(),
+                    Product = product
+                };
+
+                return View(categoryWithBrandVM);
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            _context.Update(product);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var product = _context.Products.Find(id);
+
+            if (product is not null)
+            {
+                _context.Remove(product);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+    }
+}
---
+++ ECommerce515/ViewModels/CategoryWithBrandVM.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce515.ViewModels
+{
+    public class CategoryWithBrandVM
+    {
+        public List<Category> Categories { get; set; } = null!;
+        public List<Brand> Brands { get; set; } = null!;
+        public Product? Product { get; set; }
+    }
+}
