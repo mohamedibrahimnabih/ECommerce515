06b9bf5ae76dc864f097dc56fdcb5ff7ee8607a5
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-17 20:31:26 +0300

    Admin Template

---
+++ ECommerce515/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce515.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
