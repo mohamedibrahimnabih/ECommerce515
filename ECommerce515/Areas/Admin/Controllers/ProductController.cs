0fe8fe8e1c7c0a8327785dd48c5cea4247af1021
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-21 20:51:28 +0300

    Upload Files

--- ECommerce515/Areas/Admin/Controllers/ProductController.cs
+++ ECommerce515/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce515.Areas.Admin.Controllers
@@ -31,12 +32,30 @@ namespace ECommerce515.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public IActionResult Create(Product product, IFormFile mainImg)
         {
-            _context.Add(product);
-            _context.SaveChanges();
+            if (mainImg is not null && mainImg.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(mainImg.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
 
-            return RedirectToAction(nameof(Index));
+                // Save img in wwwroot
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    mainImg.CopyTo(stream);
+                }
+
+                // Save img in DB
+                product.MainImg = fileName;
+
+                // Save product in DB
+                _context.Add(product);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return BadRequest();
         }
 
         [HttpGet]
@@ -63,12 +82,46 @@ namespace ECommerce515.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Edit(Product product)
+        public IActionResult Edit(Product product, IFormFile? mainImg)
         {
-            _context.Update(product);
-            _context.SaveChanges();
+            var productInDB = _context.Products.AsNoTracking().FirstOrDefault(e => e.ProductId == product.ProductId);
 
-            return RedirectToAction(nameof(Index));
+            if (productInDB is not null)
+            {
+                if (mainImg is not null && mainImg.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(mainImg.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
+
+                    // Save img in wwwroot
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        mainImg.CopyTo(stream);
+                    }
+
+                    // Delete old img from wwwroot
+                    var oldFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", productInDB.MainImg);
+                    if (System.IO.File.Exists(oldFilePath))
+                    {
+                        System.IO.File.Delete(oldFilePath);
+                    }
+
+                    // Save img in DB
+                    product.MainImg = fileName;
+                }
+                else
+                {
+                    product.MainImg = productInDB.MainImg;
+                }
+
+                // Update img in DB
+                _context.Update(product);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
         }
 
         public IActionResult Delete([FromRoute] int id)
